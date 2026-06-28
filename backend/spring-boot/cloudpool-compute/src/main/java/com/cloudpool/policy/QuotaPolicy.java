506532ab15fc86f4d59b7a01b085c9fe83b83eef
Mr-Charvaka <ababab01114@gmail.com>
2026-06-28 23:16:43 +0530

    Implement feature or fix in QuotaPolicy.java

--- backend/spring-boot/cloudpool-compute/src/main/java/com/cloudpool/policy/QuotaPolicy.java
+++ backend/spring-boot/cloudpool-compute/src/main/java/com/cloudpool/policy/QuotaPolicy.java
@@ -14,7 +14,7 @@ public class QuotaPolicy {
 
     public void enforceContainerQuota(User user) {
         // Enforce a hard limit of 5 containers for free-tier users
-        long containerCount = containerDeploymentRepository.countByUser(user);
+        long containerCount = containerDeploymentRepository.countByUserId(user.getId());
         if (containerCount >= 5) {
             throw new CloudPoolException("Quota Exceeded: Free tier users are limited to 5 concurrent containers. Please upgrade your plan to deploy more.");
         }
